1675be4c2a3e1023529277afdb482722f1c36773
Eduard <hadiev.e@yandex.ru>
2023-08-14 23:42:17 +0300

    style: pull request

--- src/main/java/ru/yandex/practicum/filmorate/custom_exceptions/FriendshipException.java
+++ src/main/java/ru/yandex/practicum/filmorate/custom_exceptions/FriendshipException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.custom_exceptions;
 
-public class FriendshipException extends RuntimeException{
+public class FriendshipException extends RuntimeException {
 
     public FriendshipException(String message) {
         super(message);
